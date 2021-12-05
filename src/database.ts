b53b677d93360300aac39224037bde87226f4ea7
enkitaa <ankkita95@gmail.com>
2021-12-05 19:21:07 +0530

    refactor: Add connection detail for test env

--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,32 @@
-import dotenv from 'dotenv';
-import {Pool} from 'pg';
+import dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config();
 
 const {
-    POSTGRES_HOST,
-    POSTGRES_PASSWORD,
-    POSTGRES_USER,
-    POSTGRES_DB,
-} = process.env
+  POSTGRES_HOST,
+  POSTGRES_PASSWORD,
+  POSTGRES_USER,
+  POSTGRES_DB,
+  POSTGRES_TEST_DB,
+  ENV,
+} = process.env;
 
-const client = new Pool({
+let client: Pool;
+if (ENV == "dev") {
+  client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_PASSWORD,
     user: POSTGRES_USER,
     password: POSTGRES_DB
-})
+  });
+} else {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_PASSWORD,
+    user: POSTGRES_USER,
+    password: POSTGRES_TEST_DB
+  });
+}
 
 export default client;
