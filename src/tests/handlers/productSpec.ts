02a05509cb8b9e0970e8049d3475377e535d89ee
enkitaa <ankkita95@gmail.com>
2021-12-08 00:12:12 +0530

    test: Add test for product endpoints

---
+++ src/tests/handlers/productSpec.ts
@@ -0,0 +1,68 @@
+import app from "../../server";
+import supertest from "supertest";
+import { User } from "../../models/user";
+import { Product } from "../../models/product";
+
+const request = supertest(app);
+
+describe("Tests endpoint responses for /products", () => {
+  const user: User = {
+    first_name: "Ankita",
+    last_name: "Singh",
+    password: "pass123",
+  };
+
+  const product: Product = {
+    name: "Fridge",
+    price: 900,
+  };
+
+  let jwtToken: string;
+
+  beforeAll(async () => {
+    const response = await request.post("/users").send(user);
+    jwtToken = response.body.token;
+  });
+
+  it("should create product", async () => {
+    const response = await request
+      .post("/products")
+      .send(product)
+      .set("Authorization", "bearer " + jwtToken);
+    expect(response.status).toBe(200);
+  });
+
+  it("should get products", async () => {
+    const response = await request
+      .get("/products")
+      .set("Authorization", "bearer " + jwtToken);
+    expect(response.status).toBe(200);
+  });
+
+  it("should get product with ID", async () => {
+    const response = await request
+      .get("/products/1")
+      .set("Authorization", "bearer " + jwtToken);
+    expect(response.status).toBe(200);
+  });
+
+  it("should update product", async () => {
+    const response = await request
+      .put("/products/1")
+      .send(product)
+      .set("Authorization", "bearer " + jwtToken);
+    expect(response.status).toBe(200);
+  });
+
+  it("should not delete product without required token", async () => {
+    const response = await request.delete("/products/1");
+    expect(response.status).toBe(401);
+  });
+
+  it("should delete products with token", async () => {
+    const response = await request
+      .delete("/products/1")
+      .set("Authorization", "bearer " + jwtToken);
+    expect(response.status).toBe(200);
+  });
+});
