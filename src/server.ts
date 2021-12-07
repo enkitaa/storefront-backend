275752446e2a719c06214547b7828193e6efd5fd
enkitaa <ankkita95@gmail.com>
2021-12-07 19:23:42 +0530

    feat: Add routes in server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import userRoutes from './handlers/user';
 import productRoutes from './handlers/product';
+import orderRoutes from './handlers/order';
+import orderProductRoutes from './handlers/order-products';
 
 const app: express.Application = express()
 const address: string = "http://localhost:3000"
@@ -14,6 +16,8 @@ app.get('/', function (req: Request, res: Response) {
 
 userRoutes(app);
 productRoutes(app);
+orderRoutes(app);
+orderProductRoutes(app);
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
